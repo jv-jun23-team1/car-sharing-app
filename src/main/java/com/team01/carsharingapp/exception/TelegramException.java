cdcb4a372f39b440317d732545bf04ae0d8a0ef3
Влад <dakadofordisluv@gmail.com>
2023-10-02 15:55:47 +0300

    add NotificationService

---
+++ src/main/java/com/team01/carsharingapp/exception/TelegramException.java
@@ -0,0 +1,7 @@
+package com.team01.carsharingapp.exception;
+
+public class TelegramException extends RuntimeException {
+    public TelegramException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/team01/carsharingapp/service/NotificationService.java
@@ -0,0 +1,5 @@
+package com.team01.carsharingapp.service;
+
+public interface NotificationService {
+    void sedNotification(String string);
+}
