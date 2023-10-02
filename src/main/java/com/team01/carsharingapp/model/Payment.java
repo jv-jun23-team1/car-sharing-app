f531170d5ee1958dcd1b5eaae7e3a97a13371985
Mykyta Zuber <nikitazuber@gmail.com>
2023-10-02 20:33:53 +0300

    finished payment entity

--- src/main/java/com/team01/carsharingapp/model/Payment.java
+++ src/main/java/com/team01/carsharingapp/model/Payment.java
@@ -2,13 +2,20 @@ package com.team01.carsharingapp.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import java.math.BigDecimal;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
 
@@ -16,20 +23,39 @@ import org.hibernate.annotations.Where;
 @Entity
 @SQLDelete(sql = "UPDATE Payment SET is_deleted = true WHERE id=?")
 @Where(clause = "is_deleted=false")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "payment")
 public class Payment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @OneToOne
-    @Column(name = "rental_id")
+    @Column(name = "rental_id", nullable = false)
     private Rental rental;
-    @Column(name = "session_url")
+    @Column(name = "session_url", nullable = false)
     private String urlSession;
-    @Column(name = "session_id")
+    @Column(name = "session_id", nullable = false)
     private Long sessionId;
-    @Column(name = "price")
+    @Column(name = "price", nullable = false)
     private BigDecimal price;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
+    private Status status;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "type", nullable = false)
+    private Type type;
     @Column(name = "is_deleted", nullable = false)
     private boolean isDeleted = false;
+
+    static enum Status {
+        PENDING,
+        PAID
+    }
+
+    static enum Type {
+        PAYMENT,
+        FINE
+    }
 }
