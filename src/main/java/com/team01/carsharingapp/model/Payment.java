e026f6e160c4d247ca994d4b08464c63480a7f8b
Mykyta Zuber <nikitazuber@gmail.com>
2023-10-02 14:43:38 +0300

    [PAYMENT] add entity payment

---
+++ src/main/java/com/team01/carsharingapp/model/Payment.java
@@ -0,0 +1,33 @@
+package com.team01.carsharingapp.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.math.BigDecimal;
+import lombok.Data;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+@Data
+@Entity
+@SQLDelete(sql = "UPDATE Payment SET is_deleted = true WHERE id=?")
+@Where(clause = "is_deleted=false")
+@Table(name = "payment")
+public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "rental_id")
+    private Long rentalId;
+    @Column(name = "session_url")
+    private String urlSession;
+    @Column(name = "session_id")
+    private Long sessionId;
+    @Column(name = "price")
+    private BigDecimal price;
+    @Column(name = "is_deleted", nullable = false)
+    private boolean isDeleted = false;
+}
