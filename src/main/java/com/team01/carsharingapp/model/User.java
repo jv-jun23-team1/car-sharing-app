469b247f52e09ffa07382174939da7e67b253392
Ihor Kalaur <44139757+IhorKalaur@users.noreply.github.com>
2023-10-02 18:19:57 +0300

    [User] update user model

--- src/main/java/com/team01/carsharingapp/model/User.java
+++ src/main/java/com/team01/carsharingapp/model/User.java
@@ -35,24 +35,19 @@ import org.springframework.security.core.userdetails.UserDetails;
 public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
     private Long id;
 
-    @Column(name = "first_name",
-            nullable = false)
+    @Column(nullable = false)
     private String firstName;
 
-    @Column(name = "last_name",
-            nullable = false)
+    @Column(nullable = false)
     private String lastName;
 
-    @Column(name = "email",
-            nullable = false,
+    @Column(nullable = false,
             unique = true)
     private String email;
 
-    @Column(name = "password",
-            nullable = false)
+    @Column(nullable = false)
     private String password;
 
     @ToStringExclude
@@ -65,8 +60,7 @@ public class User implements UserDetails {
     )
     private Set<Role> roles;
 
-    @Column(name = "is_deleted",
-            nullable = false)
+    @Column(nullable = false)
     private boolean isDeleted = false;
 
     @Override
