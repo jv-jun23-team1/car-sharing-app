2e4e658671e81fdea5668221867ef15211cbe8fa
Mykyta Zuber <nikitazuber@gmail.com>
2023-10-03 19:40:45 +0300

    codestyle fixed

--- src/main/java/com/team01/carsharingapp/dto/payment/PaymentRequestDto.java
+++ src/main/java/com/team01/carsharingapp/dto/payment/PaymentRequestDto.java
@@ -8,7 +8,8 @@ public record PaymentRequestDto(@NotBlank(message = "id can't be null")
                                 @Min(value = 0, message = "id can't be less than 0")
                                 Long rentalId,
                                 @NotBlank(message = "currency can't be null")
-                                @Size(min = 2, max = 3, message = "currency size can't be less than 2 and more than 3")
+                                @Size(min = 2, max = 3,
+                                        message = "currency size can't be less than 2 and more than 3")
                                 String currency,
                                 @NotBlank(message = "type can't be null")
                                 String type) {
