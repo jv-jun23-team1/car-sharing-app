c8f9e4279a855a516ba66b31b25325ffc2f173cc
Mykyta Zuber <nikitazuber@gmail.com>
2023-10-03 19:39:32 +0300

    test commit

--- src/main/java/com/team01/carsharingapp/service/impl/StripeServiceImpl.java
+++ src/main/java/com/team01/carsharingapp/service/impl/StripeServiceImpl.java
@@ -28,9 +28,6 @@ public class StripeServiceImpl implements StripeService {
                 .setCancelUrl("cancelUrl")
                 .setCurrency(currency)
                 .build();
-
-        PaymentIntentCreateParams params1 = PaymentIntentCreateParams.builder()
-                .setPa
         try {
             Session session = Session.create(params);
         }
