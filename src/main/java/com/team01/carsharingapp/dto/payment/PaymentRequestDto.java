bb6ae9222b2db4b71721290a452d1a7fc97904f6
Mykyta Zuber <nikitazuber@gmail.com>
2023-10-03 17:45:05 +0300

    fixed code

--- src/main/java/com/team01/carsharingapp/dto/payment/PaymentRequestDto.java
+++ src/main/java/com/team01/carsharingapp/dto/payment/PaymentRequestDto.java
@@ -1,13 +1,15 @@
 package com.team01.carsharingapp.dto.payment;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
-public record PaymentRequestDto(@NotBlank
+public record PaymentRequestDto(@NotBlank(message = "id can't be null")
+                                @Min(value = 0, message = "id can't be less than 0")
                                 Long rentalId,
-                                @NotBlank
-                                @Size(min = 2, max = 3)
+                                @NotBlank(message = "currency can't be null")
+                                @Size(min = 2, max = 3, message = "currency size can't be less than 2 and more than 3")
                                 String currency,
-                                @NotBlank
+                                @NotBlank(message = "type can't be null")
                                 String type) {
 }
