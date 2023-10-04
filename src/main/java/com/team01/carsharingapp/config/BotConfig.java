30854425ca5509df1be064d31b2d85198553576b
Denis Balako <denisarius2000s@gmail.com>
2023-10-04 11:09:33 +0300

    add default test

--- src/main/java/com/team01/carsharingapp/config/BotConfig.java
+++ src/main/java/com/team01/carsharingapp/config/BotConfig.java
@@ -5,14 +5,12 @@ import com.team01.carsharingapp.telegramapi.Bot;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
-@PropertySource("application.properties")
 @Configuration
 public class BotConfig {
     @Value(value = "${bot.name}") private String botName;
--- src/test/java/com/team01/carsharingapp/CarSharingAppApplicationTests.java
+++ src/test/java/com/team01/carsharingapp/CarSharingAppApplicationTests.java
@@ -1,7 +1,11 @@
 package com.team01.carsharingapp;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CarSharingAppApplicationTests {
+    @Test
+    void contextLoads() {
+    }
 }
