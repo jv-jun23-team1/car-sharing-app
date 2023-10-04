77cadf794400dd7abf5d3ce1508a318dd198322f
Mykyta Zuber <nikitazuber@gmail.com>
2023-10-04 16:40:51 +0300

    final stipe payment impl

---
+++ src/main/java/com/team01/carsharingapp/exception/PaymentException.java
@@ -0,0 +1,8 @@
+package com.team01.carsharingapp.exception;
+
+import com.stripe.exception.StripeException;
+
+public class PaymentException extends RuntimeException {
+    public PaymentException(String s, StripeException e) {
+    }
+}
