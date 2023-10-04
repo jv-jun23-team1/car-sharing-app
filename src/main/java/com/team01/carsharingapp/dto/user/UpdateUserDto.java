25706f8d9eccc3f7522cb605156ff629391431e7
Ihor Kalaur <44139757+IhorKalaur@users.noreply.github.com>
2023-10-04 12:33:24 +0300

    [User]: delete commented code

--- src/main/java/com/team01/carsharingapp/dto/user/UpdateUserDto.java
+++ src/main/java/com/team01/carsharingapp/dto/user/UpdateUserDto.java
@@ -7,25 +7,19 @@ import lombok.Data;
 
 @Data
 public class UpdateUserDto {
-    //@NotBlank(message = "required field")
-    //@Null
     @NotBlankIfNotNull
     @Size(min = 4, max = 255, message = "size must be between 4 and 255 character")
     @Email(message = "invalid email format")
     private String email;
 
-    //@NotBlank(message = "required field")
-    //@Null
     @NotBlankIfNotNull
     @Size(min = 8, max = 50, message = "size must be between 8 and 50 character")
     private String password;
 
-    //@Null
     @NotBlankIfNotNull
     @Size(min = 2, max = 50, message = "size must be between 2 and 50 character")
     private String firstName;
 
-    //@Null
     @NotBlankIfNotNull
     @Size(min = 2, max = 50, message = "size must be between 2 and 50 character")
     private String lastName;
