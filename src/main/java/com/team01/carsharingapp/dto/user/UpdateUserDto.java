cbcd36f1b8ed7e7479f19614c7b87cf2f215c992
Ihor Kalaur <44139757+IhorKalaur@users.noreply.github.com>
2023-10-04 13:26:02 +0300

    [SECURITY]: delete annotation

--- src/main/java/com/team01/carsharingapp/dto/user/UpdateUserDto.java
+++ src/main/java/com/team01/carsharingapp/dto/user/UpdateUserDto.java
@@ -1,26 +1,26 @@
 package com.team01.carsharingapp.dto.user;
 
-import com.team01.carsharingapp.validator.NotBlankIfNotNull;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class UpdateUserDto {
-    @NotBlankIfNotNull(message = "email can't be blank if not null")
+    @NotBlank(message = "required field")
     @Size(min = 4, max = 255, message = "size must be between 4 and 255 character")
     @Email(message = "invalid email format")
     private String email;
 
-    @NotBlankIfNotNull(message = "password can't be blank if not null")
+    @NotBlank(message = "required field")
     @Size(min = 8, max = 50, message = "size must be between 8 and 50 character")
     private String password;
 
-    @NotBlankIfNotNull(message = "firstName can't be blank if not null")
+    @NotBlank(message = "required field")
     @Size(min = 2, max = 50, message = "size must be between 2 and 50 character")
     private String firstName;
 
-    @NotBlankIfNotNull(message = "lastName can't be blank if not null")
+    @NotBlank(message = "required field")
     @Size(min = 2, max = 50, message = "size must be between 2 and 50 character")
     private String lastName;
 
