9c7ee31b3dbb4c64abd53b237c8f959c30b58ef0
Vlad <135035436+keda-vlad@users.noreply.github.com>
2023-10-05 18:05:32 +0300

    Update CarService.java

--- src/main/java/com/team01/carsharingapp/service/CarService.java
+++ src/main/java/com/team01/carsharingapp/service/CarService.java
@@ -3,12 +3,11 @@ package com.team01.carsharingapp.service;
 import com.team01.carsharingapp.dto.car.request.CreateCarRequestDto;
 import com.team01.carsharingapp.dto.car.response.CarDto;
 import java.util.List;
-import org.springframework.data.domain.Pageable;
 
 public interface CarService {
     CarDto save(CreateCarRequestDto request);
 
-    List<CarDto> getAll(Pageable pageable);
+    List<CarDto> getAll();
 
     CarDto getById(Long id);
 
